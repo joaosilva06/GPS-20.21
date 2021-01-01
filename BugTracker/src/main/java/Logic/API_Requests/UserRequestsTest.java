f9f355d9779b3c625f540f400c14b7117c795930
Fabio Moreira Santos <fabiosantos00.24@gmail.com>
2021-01-01 15:56:19 +0000

    Tests

---
+++ BugTracker/src/main/java/Logic/API_Requests/UserRequestsTest.java
@@ -0,0 +1,40 @@
+package Logic.API_Requests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRequestsTest {
+
+    @Test
+    void registar() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void logoff() {
+    }
+
+    @Test
+    void projects() {
+    }
+
+    @Test
+    void resetPass() {
+    }
+
+    @Test
+    void rename() {
+    }
+
+    @Test
+    void repass() {
+    }
+
+    @Test
+    void search() {
+    }
+}
