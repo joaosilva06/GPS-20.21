774f47610c4eb64f5cdd605a4f2eb6486b443e93
Gonçalo Monteiro <33238530+goncalomonteiro@users.noreply.github.com>
2021-01-04 02:48:21 +0000

    Update ProjectRequests.java

--- BugTracker/src/main/java/Logic/API_Requests/ProjectRequests.java
+++ BugTracker/src/main/java/Logic/API_Requests/ProjectRequests.java
@@ -1,7 +1,8 @@
 package Logic.API_Requests;
 
+import Logic.Bug;
 import Logic.Exceptions.APIResponseException;
-import Logic.Project;
+import Logic.Module;
 import Logic.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -11,42 +12,15 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class UserRequests {
+public class ProjectRequests{
 
-    public static User registar(String username, String password, String email) throws IOException {
-        URL url = new URL("http://localhost/GPS_BT/index.php/Update/user/register");
-        String params = "uName="+username+"&email="+email+"&pass="+password;
+    public static List<Bug> projectBugs(int project_id) throws IOException, APIResponseException {
 
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setDoOutput(true);
-        con.setRequestMethod("POST");
-        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
-        con.setRequestProperty( "charset", "utf-8");
-        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
-        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
-            wr.write(params.getBytes(StandardCharsets.UTF_8) );
-        }
-        InputStream in = con.getInputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            User rUser = mapper.readValue(in, User.class);
-            return rUser;
-        } catch (Exception e){
-            throw new IOException(e.getMessage());
-        }finally{
-            in.close();
-        }
-    }
+        URL url = new URL("http://localhost/GPS_BT/index.php/get/project/bugs");
 
-
-    public static User login(String username, String password) throws IOException {
-        URL url = new URL("http://localhost/GPS_BT/index.php/get/user/login");
-        String params = "uName="+username+"&pass="+password;
+        String params = "projId="+project_id;
 
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setDoOutput(true);
@@ -60,28 +34,263 @@ public class UserRequests {
         InputStream in = con.getInputStream();
         ObjectMapper mapper = new ObjectMapper();
         try {
-            User rUser = mapper.readValue(in, User.class);
-            return rUser;
+            List<Bug> rBug = mapper.readValue(in,  mapper.getTypeFactory().constructCollectionType(List.class, Bug.class));
+            return rBug;
         } catch (Exception e){
-            throw new IOException(e.getMessage());
+            return null;
         }finally{
             in.close();
         }
     }
 
-    public static boolean logoff() throws IOException {
-        URL url = new URL("http://localhost/GPS_BT/index.php/get/user/logoff");
+
+    public static List<User> projectMembers(int project_id) throws IOException, APIResponseException {
+
+        URL url = new URL("http://localhost/GPS_BT/index.php/get/project/members");
+
+        String params = "projId="+project_id;
 
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setDoOutput(true);
         con.setRequestMethod("POST");
         con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
         con.setRequestProperty( "charset", "utf-8");
+        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( params.getBytes(StandardCharsets.UTF_8) );
+        }
+        InputStream in = con.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            List<User> rUsers = mapper.readValue(in,  mapper.getTypeFactory().constructCollectionType(List.class, User.class));
+            return rUsers;
+        } catch (Exception e){
+            return null;
+        }finally{
+            in.close();
+        }
+    }
+
+    public static List<Module> projectModules(int project_id) throws IOException, APIResponseException {
+
+        URL url = new URL("http://localhost/GPS_BT/index.php/get/project/modules");
+
+        String params = "projId="+project_id;
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+        con.setRequestMethod("POST");
+        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty( "charset", "utf-8");
+        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( params.getBytes(StandardCharsets.UTF_8) );
+        }
+        InputStream in = con.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            List<Module> rMods = mapper.readValue(in,  mapper.getTypeFactory().constructCollectionType(List.class, Module.class));
+            return rMods;
+        } catch (Exception e){
+            return null;
+        }finally{
+            in.close();
+        }
+    }
+
+    public static boolean addProject(String project_name) throws IOException, APIResponseException {
+
+        URL url = new URL("http://localhost/GPS_BT/index.php/Update/project/add");
+
+        String params = "projName="+project_name;
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+        con.setRequestMethod("POST");
+        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty( "charset", "utf-8");
+        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( params.getBytes(StandardCharsets.UTF_8) );
+        }
         InputStream in = con.getInputStream();
         ObjectMapper mapper = new ObjectMapper();
         try {
             String resp = mapper.readValue(in, String.class);
-            if(resp.equals("Success")){
+            if(resp.equals("no rows selected") || resp.equals("Incomplete data")){
+                return false;
+            }
+            return true;
+        }catch (Exception e){
+            return false;
+        }finally{
+            in.close();
+        }
+    }
+
+    public static boolean removeProject(int project_id) throws IOException, APIResponseException {
+
+        URL url = new URL("http://localhost/GPS_BT/index.php/Update/project/remove");//nao existe, mas devia
+
+        String params = "projID="+project_id;
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+        con.setRequestMethod("POST");
+        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty( "charset", "utf-8");
+        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( params.getBytes(StandardCharsets.UTF_8) );
+        }
+        InputStream in = con.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String resp = mapper.readValue(in, String.class);
+            if(resp.equals("Erro ao remover o projeto")){
+                return false;
+            }
+            return true;
+        }catch (Exception e){
+            return false;
+        }finally{
+            in.close();
+        }
+    }
+
+    public static boolean newModule(int project_id, String moduleName) throws IOException, APIResponseException {
+
+        URL url = new URL("http://localhost/GPS_BT/index.php/Update/module/new");
+
+        String params = "projID="+project_id+"&moduleName="+moduleName;
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+        con.setRequestMethod("POST");
+        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty( "charset", "utf-8");
+        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( params.getBytes(StandardCharsets.UTF_8) );
+        }
+        InputStream in = con.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String resp = mapper.readValue(in, String.class);
+            if(resp.equals("Error adding new module") || resp.equals("register incompleto")){
+                return false;
+            }
+            return true;
+        }catch (Exception e){
+            return false;
+        }finally{
+            in.close();
+        }
+    }
+
+    public static boolean addMember(int role, int user, int proj) throws IOException, APIResponseException {
+        URL url = new URL("http://localhost/GPS_BT/index.php/Update/project/addMember");
+        String params = "role="+role+"&user="+user+"&project="+proj;
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+        con.setRequestMethod("POST");
+        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty( "charset", "utf-8");
+        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( params.getBytes(StandardCharsets.UTF_8) );
+        }
+        InputStream in = con.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String resp = mapper.readValue(in, String.class);
+            if(resp.equals("no rows selected") || resp.equals("access denied") || resp.equals("Incomplete data")){
+                return false;
+            }
+            return true;
+        }catch (Exception e){
+            return false;
+        }finally{
+            in.close();
+        }
+    }
+
+    public static boolean changeRole(int role, int user, int proj) throws IOException, APIResponseException {
+        URL url = new URL("http://localhost/GPS_BT/index.php/Update/project/role");
+        String params = "role="+role+"&user="+user+"&project="+proj;
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+        con.setRequestMethod("POST");
+        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty( "charset", "utf-8");
+        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( params.getBytes(StandardCharsets.UTF_8) );
+        }
+        InputStream in = con.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String resp = mapper.readValue(in, String.class);
+            if(resp.equals("no rows selected") || resp.equals("access denied") || resp.equals("Incomplete data")){
+                return false;
+            }
+            return true;
+        }catch (Exception e){
+            return false;
+        }finally{
+            in.close();
+        }
+    }
+
+    public static boolean removeMember(int user, int proj) throws IOException, APIResponseException {
+        URL url = new URL("http://localhost/GPS_BT/index.php/Update/project/removeMember");
+        String params = "user="+user+"&project="+proj;
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+        con.setRequestMethod("POST");
+        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty( "charset", "utf-8");
+        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( params.getBytes(StandardCharsets.UTF_8) );
+        }
+        InputStream in = con.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String resp = mapper.readValue(in, String.class);
+            if(resp.equals("no rows selected") || resp.equals("access denied") || resp.equals("Incomplete data")){
+                return false;
+            }
+            return true;
+        }catch (Exception e){
+            return false;
+        }finally{
+            in.close();
+        }
+    }
+
+    //remove module
+    public static boolean removeModule(int modId) throws IOException, APIResponseException {
+        URL url = new URL("http://localhost/GPS_BT/index.php/Update/module/remove");
+        String params = "idModule="+modId;
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+        con.setRequestMethod("POST");
+        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty( "charset", "utf-8");
+        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( params.getBytes(StandardCharsets.UTF_8) );
+        }
+        InputStream in = con.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String resp = mapper.readValue(in, String.class);
+            if(resp.equals("Deleted")){
                 return true;
             }
             return false;
@@ -91,131 +300,4 @@ public class UserRequests {
             in.close();
         }
     }
-
-    public static List<Project> projects() throws IOException {
-        URL url = new URL("http://localhost/GPS_BT/index.php/get/user/projects");
-
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setDoOutput(true);
-        con.setRequestMethod("POST");
-        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
-        con.setRequestProperty( "charset", "utf-8");
-        InputStream in = con.getInputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            List<Project> rProjs = mapper.readValue(in,  mapper.getTypeFactory().constructCollectionType(List.class, Project.class));
-            return rProjs;
-        } catch (Exception e){
-            return null;
-        }finally{
-            in.close();
-        }
-    }
-
-    public static boolean resetMail(String email) throws IOException {
-        URL url = new URL("http://localhost/GPS_BT/index.php/get/user/reset");
-        String params = "mail="+email;
-
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setDoOutput(true);
-        con.setRequestMethod("POST");
-        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
-        con.setRequestProperty( "charset", "utf-8");
-        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
-        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
-            wr.write( params.getBytes(StandardCharsets.UTF_8) );
-        }
-        InputStream in = con.getInputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            String resp = mapper.readValue(in, String.class);
-            if(resp.equals("exist")){
-                return false;
-            }
-            return true;
-        }catch (Exception e){
-            return false;
-        }finally{
-            in.close();
-        }
-    }
-
-    public static String rename(String newName) throws IOException, APIResponseException {
-        URL url = new URL("http://localhost/GPS_BT/index.php/Update/user/rename");
-        String params = "newName="+newName;
-
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setDoOutput(true);
-        con.setRequestMethod("POST");
-        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
-        con.setRequestProperty( "charset", "utf-8");
-        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
-        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
-            wr.write( params.getBytes(StandardCharsets.UTF_8) );
-        }
-        InputStream in = con.getInputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            String resp = mapper.readValue(in, String.class);
-            return resp;
-        }catch (Exception e){
-            return "Converting error";
-        }finally{
-            in.close();
-        }
-    }
-
-    public static boolean repass(String newPass) throws IOException, APIResponseException {
-        URL url = new URL("http://localhost/GPS_BT/index.php/Update/user/pass");
-        String params = "newPass="+newPass;
-
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setDoOutput(true);
-        con.setRequestMethod("POST");
-        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
-        con.setRequestProperty( "charset", "utf-8");
-        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
-        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
-            wr.write( params.getBytes(StandardCharsets.UTF_8) );
-        }
-        InputStream in = con.getInputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            String resp = mapper.readValue(in, String.class);
-            if(resp.equals("Login errado") || resp.equals("Falta de dados")){
-                return false;
-            }
-            return true;
-        }catch (Exception e){
-            return false;
-        }finally{
-            in.close();
-        }
-    }
-
-    public static User search(String search) throws IOException, APIResponseException {
-        URL url = new URL("http://localhost/GPS_BT/index.php/get/user/search");
-        String params = "search="+search;
-
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setDoOutput(true);
-        con.setRequestMethod("POST");
-        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
-        con.setRequestProperty( "charset", "utf-8");
-        con.setRequestProperty( "Content-Length", Integer.toString( params.getBytes(StandardCharsets.UTF_8).length));
-        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
-            wr.write( params.getBytes(StandardCharsets.UTF_8) );
-        }
-        InputStream in = con.getInputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            User rUser = mapper.readValue(in, User.class);
-            return rUser;
-        } catch (Exception e){
-            return null;
-        }finally{
-            in.close();
-        }
-    }
-
 }
